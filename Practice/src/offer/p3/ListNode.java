7db05ced6eefeebd3c11e785b066c1a9cc20e369
RenXin <794521557@qq.com>
2019-09-27 20:46:47 +0800

    链表逆值/逆序输出

---
+++ Practice/src/offer/p3/ListNode.java
@@ -0,0 +1,16 @@
+package offer.p3;
+
+/**
+ * @PackageName: offer.p3
+ * @ClassName: ListNode
+ * @Description:
+ * @author: 呆呆
+ * @date: 2019/9/27
+ */
+public class ListNode{
+    int val;
+    ListNode next = null;
+    ListNode(int val) {
+        this.val = val;
+    }
+}
