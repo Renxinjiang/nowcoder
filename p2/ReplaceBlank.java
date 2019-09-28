655aa0aea8668bfc09613241415e27520821fe0f
RenXin <794521557@qq.com>
2019-09-28 21:26:49 +0800

    1,2题

---
+++ p2/ReplaceBlank.java
@@ -0,0 +1,17 @@
+package offer.p2;
+
+/**
+ * @PackageName: offer.p2
+ * @ClassName: ReplaceBlank
+ * @Description: 请实现一个函数，将一个字符串中的每个空格替换成“%20”。
+ * 例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+ * @author: 呆呆
+ * @date: 2019/9/26
+ */
+public class ReplaceBlank {
+    //获取字符串中下标为i处的元素：str.charAt(i);
+    //
+    public String replaceSpace(StringBuffer str) {
+return null;
+    }
+}
